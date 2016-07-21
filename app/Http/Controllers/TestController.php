9abbb1248e588838b77ae6699b8b0beec7cae995
Bonzinho <vitor.andre.bonzinho@gmail.com>
2016-07-21 09:47:51 +0100

    Versão com foreach do blade e array vindo do controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,6 +14,19 @@ class TestController extends Controller
     }
 
     public function notas(){
-    	return view('test.notas');
+
+
+    	$notas = [
+
+    		0 => 'Anotação 1',
+    		1 => 'Anotação 3',
+    		2 => 'Anotação 4',
+    		3 => 'Anotação 5',
+    		4 => 'Anotação 6',
+
+    	];
+
+    	//return view('test.notas', ['notas' => $notas]); // Possibilidade de array 1
+    	return view('test.notas', compact('notas')); // Possibilidade de array 2
     }
 }
--- resources/views/test/notas.blade.php
+++ resources/views/test/notas.blade.php
@@ -7,8 +7,8 @@ Notas
 @section('content')
 <h1>Anotações</h1>
     <ul>
-        <li>Anotação 1</li>
-        <li>Anotação 2</li>
-        <li>Anotação 3</li>
+        @foreach($notas as $nota)
+        <li>{{$nota}}</li>
+        @endforeach
     </ul>
 @endsection
